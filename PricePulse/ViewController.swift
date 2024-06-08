af5e946ed990d32dbe7f682f22a66d84b4b2c875
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2024-06-08 12:24:22 -0400

    Initial commit

---
+++ PricePulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PricePulse
+//
+//  Created by Jacob  Loranger on 6/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
